3bd0f6728e8fa58e9b562944bcad4dbb0e08769b
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-06-27 10:38:05 -0300

    feat:  Card car component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/logo.svg';
+import { Car } from '../../components/Car';
+
+import {
+  Container,
+  Header,
+  TotalCars,
+  HeaderContent,
+} from './styles';
+
+export function Home(){
+  const carOne = {
+    brand: 'AUDI',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'AO DIA',
+      price: 120
+    },
+    thumbnail: 'https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png'
+    
+  }
+  const carTwo = {
+    brand: 'Porche',
+    name: 'Panamera',
+    rent: {
+      period: 'AO DIA',
+      price: 340
+    },
+    thumbnail: 'https://i.pinimg.com/originals/e3/99/6c/e3996cbc32b254dd28205dd7e36a6a11.png'
+    
+  }
+  return (
+    <Container>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
+      />
+      <Header>
+        <HeaderContent>
+          <Logo 
+            width={RFValue(108)}
+            height={12}
+          />
+
+          <TotalCars>
+            Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
+      </Header>
+
+      <Car data={carOne}/>
+      <Car data={carTwo}/>
+    </Container>
+  );
+};
