61617f6d7b1f43f8a36bac7517b1e35167381415
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-06-29 11:37:24 -0300

    feat: Button component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+
 import { StatusBar } from 'react-native';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
+import { Accessory } from '../../components/Accessory';
+import { Button } from '../../components/Button';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
 
 import {
   Container,
@@ -15,7 +25,9 @@ import {
   Rent,
   Period,
   Price,
-  About
+  About,
+  Accessories,
+  Footer
 } from './styles';
 
 export function CarDetails(){
@@ -33,7 +45,7 @@ export function CarDetails(){
       <CarImages>
         <ImageSlider 
             imagesUrl={['https://cdn.sitewebmotors.com.br/uploads/userGallery/5fcfe53240728.png']}
-          />
+        />
       </CarImages>
 
       <Content>
@@ -49,12 +61,28 @@ export function CarDetails(){
           </Rent>
         </Details>
 
+        <Accessories>
+          <Accessory name='380Km/h' icon={SpeedSvg}/>
+          <Accessory name='3.2s' icon={AccelerationSvg}/>
+          <Accessory name='800 HP' icon={ForceSvg}/>
+          <Accessory name='Gasolina' icon={GasolineSvg}/>
+          <Accessory name='Auto' icon={ExchangeSvg}/>
+          <Accessory name='2 pessoas' icon={PeopleSvg}/>
+        </Accessories>
+       
+
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado
           na praça Real Maestranza de Sevilla.
           É um belíssimo carro para quem gosta de acelerar.
         </About>
       </Content>
+
+      <Footer>
+        <Button 
+          title='Confirmar'
+        />
+      </Footer>
     </Container>
   );
 };
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components/native';
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getBottomSpace, getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
@@ -81,5 +81,24 @@ export const About = styled.Text`
   font-size: ${RFValue(15)}px;
   text-align: justify;
 
+  line-height: ${RFValue(25)}px;
   margin-top: 23px;
+`;
+
+export const Accessories = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+
+  margin-top: 16px;
+`;
+
+export const Footer = styled.View`
+  width: 100%;
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+
+  padding: 24px 24px ${getBottomSpace() + 24}px;
 `;
