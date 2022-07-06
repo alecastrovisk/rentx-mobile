fd499091d020820a047cb41cd85ec49b1d947f94
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-07-05 21:04:31 -0300

    feat: Add dinamic Icons

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,12 +7,8 @@ import { ImageSlider } from '../../components/ImageSlider';
 import { Accessory } from '../../components/Accessory';
 import { Button } from '../../components/Button';
 
-import SpeedSvg from '../../assets/speed.svg';
-import AccelerationSvg from '../../assets/acceleration.svg';
-import ForceSvg from '../../assets/force.svg';
-import GasolineSvg from '../../assets/gasoline.svg';
-import ExchangeSvg from '../../assets/exchange.svg';
-import PeopleSvg from '../../assets/people.svg';
+import { getAccessoryIcon } from '../../utils/getAccessoryIcon';
+
 
 import {
   Container,
@@ -85,7 +81,7 @@ export function CarDetails() {
               <Accessory
               key={accessory.type}
               name={accessory.name}
-              icon={SpeedSvg}
+              icon={getAccessoryIcon(accessory.type)}
               />
             ))   
           }
