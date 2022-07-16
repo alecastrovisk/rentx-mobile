899156ff6040de8eb0be5f6d72dd7709134c4b22
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-07-16 20:57:13 -0300

    feat: My cars component

--- src/screens/SchedulingDetails/index.tsx
+++ src/screens/SchedulingDetails/index.tsx
@@ -79,7 +79,14 @@ export function SchedulingDetails(){
       ...dates
     ];
 
-    api.put(`/schedules_bycars/${car.id}`, {
+    await api.post('/schedules_byuser', {
+      user_id: 5,
+      car,
+      startDate: format(getPlatformDate(new Date(dates[0])), 'dd/MM/yyyy'),
+      endDate: format(getPlatformDate(new Date(dates[dates.length - 1])), 'dd/MM/yyyy')
+    });
+
+    await api.put(`/schedules_bycars/${car.id}`, {
       id: car.id,
       unavailable_dates
     })
