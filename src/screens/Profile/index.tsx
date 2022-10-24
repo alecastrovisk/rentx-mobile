7a287d11af748ddc9fd0a6244fe023f8ff10ab13
Alexandre Castro <alexandre.acrsf@gmail.com>
2022-10-24 10:58:33 -0300

    feat: Sessão de cadastro

--- src/screens/Profile/index.tsx
+++ src/screens/Profile/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 
 import { BackButton } from '../../components/BackButton';
@@ -14,10 +14,15 @@ import {
   LogoutButton,
   PhotoContainer,
   Photo,
-  PhotoButton
+  PhotoButton,
+  Content,
+  Options,
+  Option,
+  OptionTitle,
 } from './styles';
 
 export function Profile(){
+  const [option, setOption] = useState<'dataEdit' | 'passwordEdit'>('dataEdit');
   const theme = useTheme();
   const navigation = useNavigation();
 
@@ -26,6 +31,10 @@ export function Profile(){
   }
 
   function handleSignOut() {
+  }
+
+  function handleOptionChange(optionSelected: 'dataEdit' | 'passwordEdit') {
+    setOption(optionSelected);
   }
 
   return (
@@ -59,6 +68,28 @@ export function Profile(){
           </PhotoButton>
         </PhotoContainer>
       </Header>
+
+      <Content>
+        <Options>
+          <Option 
+            active={option === 'dataEdit'}
+            onPress={() => handleOptionChange('dataEdit')}
+          >
+            <OptionTitle active={option === 'dataEdit'}>
+              Dados
+            </OptionTitle>
+          </Option>
+          
+          <Option
+            active={option === 'passwordEdit'}
+            onPress={() => handleOptionChange('passwordEdit')}
+          >
+            <OptionTitle active={option === 'passwordEdit'}>
+              Trocar senha
+            </OptionTitle>
+          </Option>
+        </Options>
+      </Content>
     </Container>
   );
 };
